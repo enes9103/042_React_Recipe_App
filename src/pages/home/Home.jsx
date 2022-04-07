e9e5c3524ab0390f7c0be73abfa9c0e6e1f8b4ee
EnesUnlu <enes9103@gmail.com>
2022-04-07 23:29:05 +0300

    Home added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,28 @@
-
+import axios from 'axios';
 import React from 'react'
-import Header from '../../components/header/Header'
+import Header from "../../components/header/Header";
+
+//bfbb3efc
+const APP_ID = "92380d2d";
+const APP_KEY = "43faeee790f26cd82b28050d3031619d";
+
+
 
 const Home = () => {
+
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+  
+  const getData = async () => {
+       const result=         await  axios.get(url);
+console.log(result);
+
+  }
+  
   return (
     <div>
-      <Header/>
-        
+<Header/>
+
 
 
     </div>
